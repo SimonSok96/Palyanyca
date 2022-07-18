30835790b12675b96b4407282738795cadb51653
SimonSok96 <Sokolov.sam@gmail.com>
2022-07-18 17:06:01 +0200

    blog models

---
+++ apps/blog/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+
+
+class BlogCategory(models.Model):
+    name = models.CharField(verbose_name= 'Название', max_length= 255)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Категория блога'
+        verbose_name_plural = 'Категории блогов'
+
+class Article(models.Model):
+    category = models.ForeignKey(
+        to= BlogCategory,
+        verbose_name= 'Категория',
+        on_delete= models.SET_NULL,
+        null= True
+    )
+    title = models.CharField(verbose_name= 'Заголовок', max_length=255)
+    text_previev = models.TextField(verbose_name= 'Текст-превью')
+    text = models.TextField(verbose_name= 'Текст')
+    created_ad = models.DateTimeField(verbose_name= 'Дата создания', auto_now_add= True)
+    updatet_at = models.DateTimeField(verbose_name= 'Дата редактирования', auto_now_add= True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Статья'
+        verbose_name_plural = 'Cтатьи'
