64807c387d1183093de9dfe028c02427fe2b2dbf
SimonSok96 <Sokolov.sam@gmail.com>
2022-08-03 16:18:15 +0200

    tags

--- apps/blog/models.py
+++ apps/blog/models.py
@@ -11,13 +11,26 @@ class BlogCategory(models.Model):
         verbose_name = 'Категория блога'
         verbose_name_plural = 'Категории блогов'
 
+
+class Teg(models.Model):
+    name = models.CharField(verbose_name='Название', max_length=255)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Тег'
+        verbose_name_plural = 'Теги'
+
+
 class Article(models.Model):
     category = models.ForeignKey(
         to= BlogCategory,
         verbose_name= 'Категория',
         on_delete= models.SET_NULL,
-        null= True
+        null= True,
     )
+    tegs = models.ManyToManyField(to=Teg, verbose_name='Теги')
     title = models.CharField(verbose_name= 'Заголовок', max_length=255)
     text_preview = models.TextField(verbose_name= 'Текст-превью')
     text = models.TextField(verbose_name= 'Текст')
