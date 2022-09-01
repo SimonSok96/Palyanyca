f3ddfb79c585c34b88f0a9241cd3324182e074fa
SimonSok96 <Sokolov.sam@gmail.com>
2022-09-01 17:04:06 +0200

    register

--- apps/user/forms.py
+++ apps/user/forms.py
@@ -1,6 +1,22 @@
 from django import forms
 
+from apps.user.models import User
+
 
 class LoginForm(forms.Form):
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
+
+
+class RegisterForm(forms.ModelForm):
+    password_confirm = forms.CharField(widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ['username', 'image', 'first_name', 'last_name', 'email', 'phone', 'password']
+
+    def clean_password_confirm(self):
+        cleaned_data = self.cleaned_data
+        if cleaned_data['password'] != cleaned_data['password_confirm']:
+            raise forms.ValidationError('Пароли не совподают')
+        return cleaned_data['password_confirm']
