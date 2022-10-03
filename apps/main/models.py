1a5acfc3534de4e41b73546d6eac74680e054f46
SimonSok96 <Sokolov.sam@gmail.com>
2022-10-03 18:09:25 +0300

    info pages

--- apps/main/models.py
+++ apps/main/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from tinymce.models import HTMLField
 
-# Create your models here.
+from apps.main.mixins import MetaTegMixin
+
+
+class Page(MetaTegMixin):
+    name = models.CharField(verbose_name='Название', max_length=120)
+    slug = models.SlugField(unique=True)
+    text = HTMLField(verbose_name='Описание', null=True)
+    is_active = models.BooleanField(verbose_name='Активироывно', default=True)
+
+    class Meta:
+        verbose_name = 'Информационная страница'
+        verbose_name_plural = 'Информационные страницы'
+
+    def __str__(self):
+        return self.name
